40b4243a82f4c727df818a8ca66f63f552a13e86
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-07-05 13:13:12 +0530

    cors

--- api/index.js
+++ api/index.js
@@ -6,7 +6,12 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
