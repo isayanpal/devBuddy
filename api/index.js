59893ac21fb639029b2edee8c1722d12989d5b0e
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-07-05 13:22:27 +0530

    vercel deploy

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://dev-buddy-v1.vercel.app"],
     credentials: true,
   })
 );
